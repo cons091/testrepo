6aa44c1fc521c5efa0cfe772821446666773263c
cons091 <constanzavergara011@gmail.com>
2026-02-03 09:25:20 -0500

    message

---
+++ calculateArea/groceryTracker.js
@@ -0,0 +1,12 @@
+let Grocery1;
+let Grocery2;
+let Grocery3;
+
+function calculateGrocery() {
+    Grocery1 = parseFloat(document.getElementById('Grocery1').value);
+    Grocery2 = parseFloat(document.getElementById('Grocery2').value);
+    Grocery3 = parseFloat(document.getElementById('Grocery3').value);
+
+    let Total = Grocery1 + Grocery2 + Grocery3
+    document.getElementById('result').innerText = `The total amount is:$ ${Total}`;
+}
